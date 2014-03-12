e73783a916d98e931d2994c33fd4dade05c57421
Cameron <camerondubas@gmail.com>
2014-03-12 13:53:08 -0700

    intial-snap-of-featurebranch

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngAppApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
